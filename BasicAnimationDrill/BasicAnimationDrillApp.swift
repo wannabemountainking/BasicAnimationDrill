d60a1b61552557c41da9f4e57e2f4612784db01c
wannabemountainking <kdy0096@gmail.com>
2026-07-08 16:04:21 +0900

    Initial Commit

---
+++ BasicAnimationDrill/BasicAnimationDrillApp.swift
@@ -0,0 +1,17 @@
+//
+//  BasicAnimationDrillApp.swift
+//  BasicAnimationDrill
+//
+//  Created by YoonieMac on 7/8/26.
+//
+
+import SwiftUI
+
+@main
+struct BasicAnimationDrillApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
