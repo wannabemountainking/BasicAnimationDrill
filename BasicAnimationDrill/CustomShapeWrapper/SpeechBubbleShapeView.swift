35deb5c19106abd93414b7f7140e312bcd473e4c
wannabemountainking <kdy0096@gmail.com>
2026-07-15 17:35:57 +0900

    progressbar

--- BasicAnimationDrill/CustomShapeWrapper/SpeechBubbleShapeView.swift
+++ BasicAnimationDrill/CustomShapeWrapper/SpeechBubbleShapeView.swift
@@ -8,8 +8,33 @@
 import SwiftUI
 
 struct SpeechBubbleShapeView: View {
+	
+	@State private var isTailInTrailing: Bool = false
+	@State private var isCornerRadiusLarge: Bool = false
+	
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+		VStack(spacing: 50) {
+			SpeechBubbleShape(
+				cornerRadius: isCornerRadiusLarge ? 30 : 7,
+				tailPosition: isTailInTrailing ? 0.8 : 0.2
+			)
+			.fill(Color.orange.opacity(0.2))
+			.frame(width: 150, height: 100, alignment: .center)
+			.overlay(alignment: .center) {
+				Text("안녕하세요!")
+			}
+			.animation(.easeInOut(duration: 1), value: isTailInTrailing)
+			.animation(.easeInOut(duration: 1), value: isCornerRadiusLarge)
+			
+			Button("Move Tail") {
+				isTailInTrailing.toggle()
+			}
+			
+			Button("Change CornerRadius") {
+				isCornerRadiusLarge.toggle()
+			}
+		}
     }
 }
 
@@ -28,83 +53,76 @@ struct SpeechBubbleShape: Shape {
 		}
 	}
 	
+	
 	func path(in rect: CGRect) -> Path {
+//		let startPoint: CGPoint = CGPoint(
+//			x: rect.minX + cornerRadius,
+//			y: rect.minY
+//		)
 		
-		var path = Path()
+		var rectPath = RoundedRectangle(cornerRadius: cornerRadius).path(in: rect)
+//
+//		let leadingX = rect.minX
+//		let trailingX = rect.maxX
+//		let topY = rect.minY
+//		let bottomY = rect.maxY
+//		
+//		let topLeadingArcCenter = CGPoint(x: leadingX + cornerRadius, y: topY + cornerRadius)
+//		let topTrailingArcCenter = CGPoint(x: trailingX - cornerRadius, y: topY + cornerRadius)
+//		let bottomTrailingArcCenter = CGPoint(x: trailingX - cornerRadius, y: bottomY - cornerRadius)
+//		let bottomLeadingArcCenter = CGPoint(x: leadingX + cornerRadius, y: bottomY - cornerRadius)
+//		
+//		var rectPath = Path()
+//		rectPath.move(to: startPoint)
+//		rectPath.addLine(to: CGPoint(x: trailingX - cornerRadius, y: topY))
+//		rectPath.addArc(
+//			center: topTrailingArcCenter,
+//			radius: cornerRadius,
+//			startAngle: Angle(degrees: -90),
+//			endAngle: Angle(degrees: 0),
+//			clockwise: false
+//		)
+//		rectPath.addLine(to: CGPoint(x: trailingX, y: bottomY - cornerRadius))
+//		rectPath.addArc(
+//			center: bottomTrailingArcCenter,
+//			radius: cornerRadius,
+//			startAngle: Angle(degrees: 0),
+//			endAngle: Angle(degrees: 90),
+//			clockwise: false
+//		)
+//		rectPath.addLine(to: CGPoint(x: leadingX + cornerRadius, y: bottomY))
+//		rectPath.addArc(
+//			center: bottomLeadingArcCenter,
+//			radius: cornerRadius,
+//			startAngle: Angle(degrees: 90),
+//			endAngle: Angle(degrees: 180),
+//			clockwise: false
+//		)
+//		rectPath.addLine(to: CGPoint(x: leadingX, y: topY + cornerRadius))
+//		rectPath.addArc(
+//			center: topLeadingArcCenter,
+//			radius: cornerRadius,
+//			startAngle: Angle(degrees: 180),
+//			endAngle: Angle(degrees: 270),
+//			clockwise: false
+//		)
+//		rectPath.closeSubpath()
 		
-		return path
+		var trianglePath = Path()
+		let tailX = rect.minX + (tailPosition * rect.width)
+		let triStartingPoint = CGPoint(x: tailX - 10 , y: rect.maxY)
+		let triSecondPoint = CGPoint(x: tailX + 10, y: rect.maxY)
+		let triPointedPoint = CGPoint(x: tailX, y: rect.maxY + 20)
+		
+		trianglePath.move(to: triStartingPoint)
+		trianglePath.addLine(to: triSecondPoint)
+		trianglePath.addLine(to: triPointedPoint)
+		trianglePath.closeSubpath()
+		
+		rectPath.addPath(trianglePath)
+		
+		return rectPath
 	}
-	
-	private func roundedRectanglePath(in rect: CGRect) -> Path {
-		
-		let startPoint: CGPoint = CGPoint(
-			x: rect.minX + cornerRadius,
-			y: rect.minY
-		)
-		let leadingX = rect.minX
-		let trailingX = rect.maxX
-		let topY = rect.minY
-		let bottomY = rect.maxY
-		
-		let topLeadingArcCenter = CGPoint(x: leadingX + cornerRadius, y: topY + cornerRadius)
-		let topTrailingArcCenter = CGPoint(x: trailingX - cornerRadius, y: topY + cornerRadius)
-		let bottomTrailingArcCenter = CGPoint(x: trailingX - cornerRadius, y: bottomY - cornerRadius)
-		let bottomLeadingArcCenter = CGPoint(x: leadingX + cornerRadius, y: bottomY - cornerRadius)
-		
-		var path = Path()
-		path.move(to: startPoint)
-		path.addLine(to: CGPoint(x: trailingX - cornerRadius, y: topY))
-		path.addArc(
-			center: topTrailingArcCenter,
-			radius: cornerRadius,
-			startAngle: Angle(degrees: -90),
-			endAngle: Angle(degrees: 0),
-			clockwise: false
-		)
-		path.addLine(to: CGPoint(x: trailingX, y: bottomY - cornerRadius))
-		path.addArc(
-			center: bottomTrailingArcCenter,
-			radius: cornerRadius,
-			startAngle: Angle(degrees: 0),
-			endAngle: Angle(degrees: 90),
-			clockwise: false
-		)
-		path.addLine(to: CGPoint(x: leadingX + cornerRadius, y: bottomY))
-		path.addArc(
-			center: bottomLeadingArcCenter,
-			radius: cornerRadius,
-			startAngle: Angle(degrees: 90),
-			endAngle: Angle(degrees: 180),
-			clockwise: false
-		)
-		path.addLine(to: CGPoint(x: leadingX, y: topY + cornerRadius))
-		path.addArc(
-			center: topLeadingArcCenter,
-			radius: cornerRadius,
-			startAngle: Angle(degrees: 180),
-			endAngle: Angle(degrees: 270),
-			clockwise: false
-		)
-		path.closeSubpath()
-		return path
-	}
-	
-	private func reverseTriangle(in rect: CGRect) -> Path {
-		let tailX = rect.midX + (tailPosition * rect.width)
-		let startingPoint = CGPoint(x: tailX - 10 , y: rect.minY)
-		let secondPoint = CGPoint(x: rect.maxX, y: rect.minY)
-		let pointedPoint = CGPoint(x: tailX, y: rect.maxY)
-		
-		var path = Path()
-		path.move(to: startingPoint)
-		path.addLine(to: secondPoint)
-		path.addLine(to: pointedPoint)
-		path.addLine(to: startingPoint)
-		path.closeSubpath()
-		
-		return path
-	}
-	
 }
 
 #Preview {
---
+++ BasicAnimationDrill/GeometryReadrMaskView/LinearProgressBarView.swift
@@ -0,0 +1,32 @@
+//
+//  LinearProgressBarView.swift
+//  BasicAnimationDrill
+//
+//  Created by YoonieMac on 7/15/26.
+//
+
+import SwiftUI
+
+struct LinearProgressBarView: View {
+	
+	@State private var barWidth: CGFloat = 0
+	
+    var body: some View {
+		ZStack {
+			RoundedRectangle(cornerRadius: 8)
+				.fill(Color.gray.opacity(0.2))
+				.frame(height: 20)
+				.frame(maxWidth: .infinity)
+				.overlay(alignment: .leading) {
+					Ge
+					RoundedRectangle(cornerRadius: 8)
+						.fill(.accent)
+						
+				}
+		}
+    }
+}
+
+#Preview {
+    LinearProgressBarView()
+}
